9797a9aabb36d3671079b871206bdc6db1b75253
Aitor Alejandro <aitor.alejandro.herrera@gmail.com>
2021-03-11 20:36:37 +0100

    calculadora sumar y restar

---
+++ calculadora/calculadora.py
@@ -0,0 +1,13 @@
+def sumar(numero1, numero2):
+  return numero1 + numero2
+
+def restar(numero1,  numero2):
+  return numero1 - numero2
+
+print("---------- CALCULADORA -----------")
+
+print(sumar(3,4))
+print(sumar(6754,123412341231234123412341234))
+
+print(restar(9000000006543210000,900000000012345678))
+print(restar(100,50))
--- calculadora/sumar.py
+++
@@ -1,6 +0,0 @@
-def sumar(numero1, numero2):
-    return numero1 + numero2
-
-print( sumar(43, 88) )
-print( sumar(89765, 754322) )
-
