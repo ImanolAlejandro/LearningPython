0fcf545f7e46f006d37b6fb851ede2d8835d042b
Aitor Alejandro Herrera <aitor.alejandro.herrera@gmail.com>
2021-11-27 20:07:30 +0100

    data types

---
+++ variables/data_types.py
@@ -0,0 +1,21 @@
+# variables de tipo número
+contador = 10
+edad = 18
+
+# variables de tipo texto
+texto = "mundo"
+saludo = "hola \nque tal\a\a\a"
+print(saludo)
+
+# variables de tipo Boolean
+mikelPesado = True
+imanolPesado = False
+
+# listas
+amigos = ["Aner","Irkus","Unax","Liher","Jon"]
+print(amigos)
+anyos=[1000000,10000000,100000000]
+print(anyos)
+
+# elegir un elemento de la lista
+print(amigos[0])
