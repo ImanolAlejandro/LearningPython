55415f5689606e607bdf559a712900879b950ee6
Aitor Alejandro Herrera <aitor.alejandro.herrera@gmail.com>
2022-03-20 19:48:35 +0100

    Create ExcitingSeconds.py

---
+++ ExcitingSeconds.py
@@ -0,0 +1,11 @@
+print("---------- Exciting Seconds ----------")
+
+numero_dias = int(input("Escribe los dias de los que quieres saber los segundos: "))
+
+segundos = 60
+minutos = 60
+horas = 24
+total_segundos = segundos * minutos * horas * numero_dias
+print("la cantidad de segundos es " + str(total_segundos))
+
+print("---------------------------------------")
